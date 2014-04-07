634798f846ef4c7cf912dfb3875099f3c5808345
rhrh1997 <RAZA@RZ-MacBook-Pro.local>
2014-04-07 12:48:15 -0500

    Remove more sidebar stuff

--- App Folder/App Link/Storyboards/PastOrderList.m
+++ App Folder/App Link/Storyboards/PastOrderList.m
@@ -37,7 +37,7 @@
 
 /*
 #pragma mark - Navigation
-
+//something something something something something
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
